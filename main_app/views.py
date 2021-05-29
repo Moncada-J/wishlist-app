3ceced4d3c1771e2d85bccb4d6c8147344672b6f
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-05-29 09:11:07 -0700

    updates table in index

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
 from .models import Item
+from .forms import ItemForm
 # Create your views here.
 
 
@@ -11,4 +13,14 @@ def items_index(request):
 class ItemCreate(CreateView):
     model = Item
     fields = '__all__'
-    success_url = ''
+    success_url = '/'
+
+
+# def add_item(request):
+#  if request.method == 'POST':
+#      form = ItemForm(request.POST)
+#      if form.is_valid():
+#          return HttpResponseRedirect('/')
+#      else:
+#         form = ItemForm()
+#         return render(request, 'add.html', {'form': form})
